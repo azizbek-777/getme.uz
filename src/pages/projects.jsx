12c6b0928a3dc08c3bdb367a2cb86526f839b2a0
Azizbek Berdimuratov <a.berdimuratov@uzinfocom.uz>
2025-02-21 09:52:09 +0500

    changed

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -12,6 +12,9 @@ import {
   SiExpress,
 } from 'react-icons/si'
 
+import { GoArrowUpRight } from "react-icons/go";
+
+
 import logoTexnopos from '@/images/logos/logo_texnopos.jpg'
 
 
@@ -48,22 +51,30 @@ const PozdravCard = () => {
       {cardData.map((card) => (
         <div key={card.id}
           className="w-[362px] rounded-2xl overflow-hidden border dark:border dark:border-zinc-800">
-
-          <div className="w-full h-[160px]">
+          <div className="w-full h-[160px] overflow-hidden">
             <Image
               src={card.image}
               alt={card.title}
               width={280}
               height={160}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover transition-transform duration-500 ease-in-out transform hover:scale-110"
               loading="lazy"
             />
           </div>
           <div className='p-[20px]'>
+
             <div>
-              <p className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {card.title}
-              </p>
+              <div className="flex items-center justify-between">
+                <p className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {card.title}
+                </p>
+                <GoArrowUpRight
+                  className="text-[#7D7D7D] text-2xl border border-[#7D7D7D] dark:border-white rounded-md 
+                          hover:text-white hover:bg-black hover:border-black dark:hover:bg-white dark:hover:text-black dark:hover:border-white 
+                          transition-colors duration-300 cursor-pointer"
+                />
+
+              </div>
               <p className="text-zinc-600 dark:text-zinc-400">
                 {card.description}
               </p>
@@ -74,7 +85,6 @@ const PozdravCard = () => {
                 { icon: SiTypescript, name: "TypeScript", url: "https://www.typescriptlang.org", color: "text-blue-600 dark:text-blue-400", hover: "hover:bg-blue-100 dark:hover:bg-blue-900" },
                 { icon: FaNodeJs, name: "Node.js", url: "https://nodejs.org", color: "text-green-600 dark:text-green-400", hover: "hover:bg-green-100 dark:hover:bg-green-900" },
                 { icon: SiNestjs, name: "NestJS", url: "https://nestjs.com", color: "text-red-600 dark:text-red-400", hover: "hover:bg-red-100 dark:hover:bg-red-900" },
-                { icon: SiExpress, name: "ExpressJS", url: "https://expressjs.com", color: "text-gray-600 dark:text-gray-400", hover: "hover:bg-gray-100 dark:hover:bg-gray-900" },
               ].map(({ icon: Icon, name, url, color, hover }, index) => (
                 <a
                   key={index}
@@ -84,8 +94,8 @@ const PozdravCard = () => {
                   className={`px-1 py-1 border border-gray-500 rounded-3xl flex items-center gap-1 transition transform 
                       ${hover} hover:border-transparent hover:scale-105`}
                 >
-                  <Icon className={`w-4 h-4 ${color}`} />
-                  <span className="font-medium text-zinc-600 dark:text-zinc-400">{name}</span>
+                  <Icon className={`w-3 ${color}`} />
+                  <span className="font-medium text-[12px] text-zinc-600 dark:text-zinc-400">{name}</span>
                 </a>
               ))}
             </div>
