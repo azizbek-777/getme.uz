9dd91dff9e29b161d9618347ea071c241ca8f368
Azizbek Berdimuratov <a.berdimuratov@uzinfocom.uz>
2025-02-23 21:44:52 +0500

    changed

--- src/constants/socials.js
+++ src/constants/socials.js
@@ -1,6 +1,6 @@
 export const SOCIALLINKS ={
-    github: "https://github.com/undefineduz",
-    linkedin: "https://www.linkedin.com/in/undefined-uz",
+    github: "https://github.com/azizbek-777",
+    linkedin: "https://www.linkedin.com/in/getmeinfo/",
     instagram: "https://www.instagram.com/dev_azizbek",
-    telegram: "https://t.me/undefined_uz",
+    telegram: "https://t.me/feat_username",
 }
